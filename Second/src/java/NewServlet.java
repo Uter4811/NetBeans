1cc0064958f89321ca0e84ad3288d69633d61763
Anton <sogonyaevjk@mail.ru>
2020-05-22 16:14:44 +0300

    next3

--- Second/src/java/NewServlet.java
+++ Second/src/java/NewServlet.java
@@ -5,45 +5,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class NewServlet extends HttpServlet {
 
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+          
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        
+        String[] body = request.getParameterValues("body");
+         
+        try {
+            writer.print("<h2>Numbers: ");
+            for(String n:  body)
+                writer.print(n + " ");
+            writer.println("</h2>");
+        } finally {
+            writer.close();  
         }
     }
-
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
