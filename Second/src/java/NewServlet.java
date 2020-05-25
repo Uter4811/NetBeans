bac5f9d15d4ca15152202dd9a57166d0cba52622
Anton <sogonyaevjk@mail.ru>
2020-05-25 11:00:09 +0300

    next4

--- Second/src/java/NewServlet.java
+++ Second/src/java/NewServlet.java
@@ -13,24 +13,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class NewServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-          
-        response.setContentType("text/html");
-        PrintWriter writer = response.getWriter();
-        
-        String[] body = request.getParameterValues("body");
-         
-        try {
-            writer.print("<h2>Numbers: ");
-            for(String n:  body)
-                writer.print(n + " ");
-            writer.println("</h2>");
-        } finally {
-            writer.close();  
-        }
+      protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream(), Charset.forName("UTF-8")));
+        String line = "";
+        StringBuilder result = new StringBuilder();
+        while ((line = in.readLine()) != null) {
+            result.append(line);
+            }
+        in.close();
+        response.setContentType ("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("Ответ на POST запрос: ");
+        pw.println(result.toString());
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType ("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Hello! ответ на GET запрос. </h1>");
+        pw.println("</html>");
     }
 }
